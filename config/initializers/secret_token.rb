d6a99eb07587dd92fe096a8323aff4ab5a35c3df
Jonathan Gertig <jcgertig@gmail.com>
2014-03-31 22:08:32 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eightbitbacklog::Application.config.secret_key_base = '578ba47e1706cf53415fff22e907ac1a1109e2024ed0f0591b392ea8e15eb27c27fd4f11ce0c90cda59860747d065af72a12ec9f1b8832cc5dc47cd8aaac8d8d'
