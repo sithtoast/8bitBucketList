69584c4d0746044d3a3efe4efb530ed0a424fb3c
Jonathan Gertig <jcgertig@gmail.com>
2014-04-01 11:35:43 -0500

    get all the detail

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,24 @@
 class PagesController < ApplicationController
   def index
   end
-  
+
   def search
-  	if params[:query]
-	  	@game = params[:query]
-	  	@search = GiantBomb::Search.new
-	  	@search.limit(10) # limits number of returned resources
-	  	@search.resources('game') # determines type(s) of resources
-	  	@search.fields('developers') # limits fields returned
-	  	@search.offset(100) # sets the offset
-	  	@search.query(@game) # the query to search against
-	  	@results = @search.fetch # makes request
-	  	binding.pry
+    if params[:query]
+      @game = params[:query]
+      @search = GiantBomb::Search.new
+      @search.limit(10) # limits number of returned resources
+      @search.resources('game') # determines type(s) of resources
+      @search.fields('id') # limits fields returned
+      @search.offset(100) # sets the offset
+      @search.query(@game) # the query to search against
+      @results = @search.fetch # makes request
+
+      @temp = []
+      @results.each do |result|
+        @resultgame = GiantBomb::Game.detail(result["id"])
+        @temp.push(@resultgame)
+      end
+      @results = @temp
   	end
   end
 end
