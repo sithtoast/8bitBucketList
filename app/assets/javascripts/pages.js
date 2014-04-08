213928fe80b2b6cef5d08a05ec3675f2490f552f
William Holt <william@toastedlabs.com>
2014-04-08 07:44:06 -0500

    Button toggles for console selections - once more

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -18,7 +18,7 @@ var values = $('input:checkbox:checked.platbtn').map(function() {
 $(".platforms input[type=checkbox]").on("change", function() {
   var selected = $(this).val(); // This will get the value
   $("."+selected).prop('checked', 'checked'); // This will check the box
-  $("."+selected).prop('checked', 'checked', false, false);
+  
 });
 
 });
