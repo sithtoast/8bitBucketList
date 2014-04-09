9b2666e198f92ade626450e0a42da8a0d094eabc
Jonathan Gertig <jcgertig@gmail.com>
2014-04-09 00:09:28 -0500

    add toggle to platform

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,14 +1,1 @@
-$(document).ready(function(){
-    $("#platforms .btn").click(function(){
-    	$(this).button('toggle');
-    });
 
-  $(".platforms input[type=checkbox]").on("change", function() {
-    var selected = $(this).val(); // This will get the value
-    if ($(this).is(':checked'))
-    $("."+selected).first().prop('checked', 'checked'); // This will check the box
-    else
-    $("."+selected).prop('checked', false);
-    });
-
-});
