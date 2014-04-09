b91e7ae0319fa143046c04c8a9746bd77bc9daad
Jonathan Gertig <jcgertig@gmail.com>
2014-04-08 23:21:35 -0500

    fix table layout

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,22 +2,9 @@ class GamesController < ApplicationController
 	def create
 		@game = Game.where(giantbomb_id: params[:game][:game_id]).first
 		current_user.gamelist.gamelistgames.create(game_id: @game.id)
-		if params[:game][:platforms][:id]
+		if params[:game][:platforms]
 		  params[:game][:platforms][:id].each do |platform|
 		    current_user.gamelist.gamelistgames.where(game_id: @game.id).first.gamelistgameplatforms.create(platform_id: platform)
-		  end
-		end
-		
-		# Do I need to do this? I would think so. None of the games show up with devs/publishers on the list.
-		if params[:game][:developers][:id]
-		  params[:game][:developers][:id].each do |developer|
-		    current_user.gamelist.gamelistgames.where(game_id: @game.id).first.gamelistgamedevelopers.create(developer_id: developer)
-		  end
-		end
-		
-		if params[:game][:publishers][:id]
-		  params[:game][:publishers][:id].each do |publisher|
-		    current_user.gamelist.gamelistgames.where(game_id: @game.id).first.gamelistgamepublishers.create(publisher_id: publisher)
 		  end
 		end
 
